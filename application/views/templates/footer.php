f800b33863229f799a419cf89372d2fd831cb731
dfarias <dafarit13@gmail.com>
2019-10-08 14:55:28 -0500

    Segunda  version

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,22 +1,81 @@
-<!-- Start Cta Area -->
-        <section class="cta-area">
-                <div class="container">
-                        
-                </div>
-        </section>
-        <!-- End Cta Area -->
-        <footer class="section-half">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3">
-                            FOOTER
-                    </div>                    
-                    
-                </div>
-                
-            </div>
-        </footer>
-    
+    <!-- Footer content -->
+    <footer class="footer mt-auto">
+      <div class="row mx-0 mb-neg30">
+        <div class="col">
+        <div class="container float-footer">
+          <div class="row text-center">
+            <div class="col">
+                <blockquote class="blockquote">Educación con calidad para todos</blockquote>
+              </div>
+          </div>
 
-</body>
+          </div>
+        </div>
+      </div>
+      <div class="row bg-primary mx-0 py-5 mid-footer">
+        <div class="col">
+            <div class="container">
+              <div class="row justify-content-md-center align-items-center pt-4 pb-2">
+                <div class="col col-lg-4 align-self-start">
+                  <h5 class="text-muted font-weight-bold">Contacto</h5>
+                  <ul class="fa-ul text-white">
+                    <li><span class="fa-li fc-1"><i class="far fa-building"></i></span>Torre Natyvo #1234</li>
+                    <li><span class="fa-li fc-1"><i class="fas fa-map-pin"></i></span>Puebla, México.</li>
+                    <li><span class="fa-li fc-1"><i class="fas fa-phone-alt"></i></span>+52 222 241 9257</li>
+                    <li><span class="fa-li fc-1"><i class="far fa-envelope"></i></span>info@proyectoeducativo.org</li>
+
+                  </ul>
+                </div>
+                <div class="col-md-auto my-auto">
+                  <span class="icon-Iso-Proyed-01 f-icon"></span>
+                </div>
+                <div class="col col-lg-4 text-right align-self-start">
+                  <div class="row text-right">
+                    <div class="col">
+                      <h5 class="text-muted font-weight-bold mb-3">Síguenos</h5>
+                      <span class="fa-stack fa-lg icon">
+                        <i class="fas fa-circle fa-stack-2x fc-1"></i>
+                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                      </span>
+                      <span class="fa-stack fa-lg icon">
+                        <i class="fas fa-circle fa-stack-2x fc-1"></i>
+                        <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                      </span>
+                      <span class="fa-stack fa-lg icon">
+                        <i class="fas fa-circle fa-stack-2x fc-1"></i>
+                        <i class="fab fa-linkedin-in fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+      </div>
+    </div>
+    <div class="row bg-dark mx-0 my-0 py-3">
+        <div class="col">
+            <div class="container text-center">
+                <span class="text-muted"><i class="fas fa-copyright mr-3"></i> Algunos derechos reservados <i class="fas fa-ellipsis-v mx-3"></i> 2019. </span>
+              </div>
+      </div>
+    </div>
+    </footer>
+    <div class="btn-whatsapp">
+    <!--<a href="https://api.whatsapp.com/send?phone=522221576243" target="_blank">-->
+    <a href="https://web.whatsapp.com/send?phone=522222419257" target="_blank" id="url">
+    <!--<img src="https://png.pngtree.com/element_our/sm/20180506/sm_5aeee502caf7f.png" alt=""> -->
+    <img src="http://s2.accesoperu.com/logos/btn_whatsapp.png" alt="">
+    </a>
+    </div>
+  </body>
+<!-- Bootstrap core JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- END Bootstrap core JS -->
+    <!-- AOS Animation -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script src="<?= base_url('assets/js/main.js'); ?>"></script>
+
 </html>
