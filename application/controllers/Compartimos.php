01ab30e4ac896f92643f96369358ed69c7f0a543
Jose Luis Sanchez <jlleluis@gmail.com>
2020-06-27 15:07:55 -0500

    funcionalidad icono

--- application/controllers/Compartimos.php
+++ application/controllers/Compartimos.php
@@ -6,6 +6,7 @@ class Compartimos extends CI_Controller {
 		function __construct() {
 			parent::__construct();
 			$this->load->library('Utilerias');
+			$this->load->library('user_agent');
 			$this->load->model( 'Informacion_apoyo_model' );
 		}
 
@@ -26,6 +27,19 @@ class Compartimos extends CI_Controller {
 	 */
 	public function index()
 	{
+		$origen = $this->agent->referrer();
+		$sarape   = 'http://localhost/eclasebas_yuc/';
+		$yucatan   = 'http://localhost/eclase/';
+		$possarape = strstr($origen, $sarape);
+		$poskambal = strstr($origen, $yucatan);
+		$imagen = "proyed";
+		if(!$possarape){
+			$imagen = "logo-sarape.png";
+		}
+
+		if(!$poskambal){
+			$imagen = "logo-kaambal.png";
+		}
 		$data = array();
 		$c_area = $this->Informacion_apoyo_model->obtener_c_area();
 		$c_nivel = $this->Informacion_apoyo_model->obtener_c_nivel();
@@ -33,6 +47,7 @@ class Compartimos extends CI_Controller {
 		$data['nivel'] = '';
 		$data['c_area'] = $c_area;
 		$data['area'] = '';
+		$data['logo'] = $imagen;
 		// Utilerias::pagina_basica($this, "index", $data);
 		Utilerias::pagina_basica($this, "compartimos/index", $data);
 
