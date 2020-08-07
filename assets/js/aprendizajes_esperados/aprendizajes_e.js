11377057f3f4dce0c481080d38c7ce8d211cb27e
Jose Luis Sanchez <jlleluis@gmail.com>
2020-08-07 11:57:54 -0500

    ajuste filtros

---
+++ assets/js/aprendizajes_esperados/aprendizajes_e.js
@@ -0,0 +1,26 @@
+$("#slc_nivel_ae").change(function(){
+	$('#slc_componente_ae').prop('disabled', false);
+});
+$("#slc_componente_ae").change(function(){
+	$('#slc_campo_ae').prop('disabled', false);
+});
+$("#slc_campo_ae").change(function(){
+	$('#slc_grado_ae').prop('disabled', false);
+});
+$("#slc_grado_ae").change(function(){
+	$('#slc_asignatura_ae').prop('disabled', false);
+});
+$("#slc_asignatura_ae").change(function(){
+	$('#slc_eje_ae').prop('disabled', false);
+});
+$("#slc_eje_ae").change(function(){
+	$('#slc_tema_ae').prop('disabled', false);
+});
+// $("#slc_tema_ae").change(function(){
+// 	$('#slc_componente_ae').prop('disabled', false);
+// });
+	
+let Aprendizajes_e = {
+
+
+}
