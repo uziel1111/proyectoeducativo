e4ba71d45724d28150f2b14863ef480f7c5b16e9
uziel1111 <uziel.123@gmail.com>
2020-08-10 13:25:29 -0500

    ajuste tabla

--- application/views/aprendizajes_esperados/index.php
+++ application/views/aprendizajes_esperados/index.php
@@ -16,17 +16,7 @@
     <div class="container pt-0">
       <div class="row" id="row-lowgray-1">
         <div class="col-lg-12 text-justify" data-aos="zoom-in">
-          <div class="row">
-            <div class="col-12 col-md-6 col-lg-6">
-              <!-- <strong>Seleccione la opción deseada:</strong> -->
-            </div>
-            <div class="col-12 col-md-6 col-lg-6" id="div_pclaves">
-            </div>
-          </div>
-          <div class="row">
-          <div class="col-lg-12"><h1>  </h1></div>
-          <div class="col-lg-12"><h1>  </h1></div>
-        </div>
+
         <form name="form_aprendizaje_esperado" id="form_aprendizaje_esperado">
           <div class="row">
             <div class="col-12 col-md-6 col-lg-2">
@@ -36,16 +26,16 @@
                 <?php foreach ($niveles as $key => $value): ?>
                   <option value='<?=$value['idnivel']?>'><?=$value['nivel']?></option>
                 <?php endforeach ?>
-                
+
               </select>
             </div>
-            <div class="col-12 col-md-6 col-lg-2">
+            <div class="col-12 col-md-6 col-lg-3">
               <label for="slc_componente_ae">Componente</label>
               <select id="slc_componente_ae" class="form-control" disabled>
                 <option value='0'>SELECCIONE</option>
               </select>
             </div>
-            <div class="col-12 col-md-6 col-lg-2">
+            <div class="col-12 col-md-6 col-lg-3">
               <label for="slc_campo_ae">Campo</label>
               <select id="slc_campo_ae" class="form-control" disabled>
                <option value='0'>SELECCIONE</option>
@@ -93,7 +83,7 @@
         </div>
         <div class="row">
          <div class="col-12" style="overflow-x:auto;" id="div_tabla_aprendizajes">
-          
+
          </div>
    </div>
  </div>
