a2d3d11f8cb885daad4fd2f90dad2e9269027ca8
Jose Luis Sanchez <jlleluis@gmail.com>
2020-08-11 12:47:52 -0500

    ajustes localstorage

--- application/views/templates_ae/header.php
+++ application/views/templates_ae/header.php
@@ -22,13 +22,7 @@
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
-  <script type="text/javascript">
-    var logo = <?=((isset($logo))?$logo:'') ?>;
-    var estilo = <?=((isset($estilo))?$estilo:'') ?>;
-    mystorage = window.localStorage;
-   localStorage.setItem('logo', logo);
-   localStorage.setItem('estilo', estilo);
-  </script>
+ 
   <!-- PLUGINS CSS STYLE -->
   <?php if (isset($estilo)): ?>
     <link rel="stylesheet" href="<?=$estilo ?>">
