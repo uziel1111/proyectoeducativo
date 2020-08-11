25fecdddfd2884ca0b65024c25da9f0d9dbde5ea
uziel1111 <uziel.123@gmail.com>
2020-08-11 13:00:11 -0500

    ajuste visuales

--- application/views/templates_ae/header.php
+++ application/views/templates_ae/header.php
@@ -22,7 +22,7 @@
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
- 
+
   <!-- PLUGINS CSS STYLE -->
   <?php if (isset($estilo)): ?>
     <link rel="stylesheet" href="<?=$estilo ?>">
@@ -83,14 +83,16 @@
                 <img src="<?= base_url('assets/' . SKIN . '/img/template/logoQualiclass150x400.png') ?>" alt="">
               </a>
             </div>
-            <?php if (isset($logo)): ?>
-              <div class="col-4">
-              <a class="navbar-brand mr-auto" href="">
-                <img src="<?= $logo ?>" alt="">
-              </a>
-            </div>
-            <?php endif ?>
+            <div class="col-4"></div>
+
           </div>
+          <?php if (isset($logo)): ?>
+            <div class="col-4 ">
+            <a class="navbar-brand float-right" href="">
+              <img src="<?= $logo ?>" alt="">
+            </a>
+          </div>
+          <?php endif ?>
         </div>
       </nav>
 
