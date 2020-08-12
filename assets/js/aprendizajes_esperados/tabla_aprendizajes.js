332b97a028906b37c21b59cd3716830109dcd713
uziel1111 <uziel.123@gmail.com>
2020-08-12 11:18:28 -0500

    ajustes tabla

--- assets/js/aprendizajes_esperados/tabla_aprendizajes.js
+++ assets/js/aprendizajes_esperados/tabla_aprendizajes.js
@@ -14,24 +14,24 @@ $(document).ready( function () {
     responsive: false,
     initComplete: function () {
       this.api().columns().every( function () {
-        var column = this;
+        // var column = this;
         // console.log($(column.header()).text().trim());
-        if ($(column.header()).text().trim()!='') {
-          var select = $('<br><select  style="width:170px"><option value="">Todos los resultados</option></select>')
-          .appendTo( $(column.header()) )
-          .on( 'change', function () {
-            var val = $.fn.dataTable.util.escapeRegex(
-              $(this).val()
-              );
-
-            column
-            .search( val ? '^'+val+'$' : '', true, false )
-            .draw();
-          } );
-          column.data().unique().sort().each( function ( d, j ) {
-            select.append( '<option>'+d+'</option>' )
-          } );
-        }
+        // if ($(column.header()).text().trim()!='') {
+        //   var select = $('<br><select  style="width:170px"><option value="">Todos los resultados</option></select>')
+        //   .appendTo( $(column.header()) )
+        //   .on( 'change', function () {
+        //     var val = $.fn.dataTable.util.escapeRegex(
+        //       $(this).val()
+        //       );
+        //
+        //     column
+        //     .search( val ? '^'+val+'$' : '', true, false )
+        //     .draw();
+        //   } );
+        //   column.data().unique().sort().each( function ( d, j ) {
+        //     select.append( '<option>'+d+'</option>' )
+        //   } );
+        // }
       } );
     }
 });
