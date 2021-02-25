a9270fdd7ec0578769f7c5ac7043e75c9c2ae3eb
uziel1111 <uziel.123@gmail.com>
2021-02-25 12:57:52 -0600

    xv años

---
+++ application/views/fotogaleria/index.php
@@ -0,0 +1,111 @@
+    <!-- Main page content -->
+    <main role="main" class="flex-shrink-0">
+      <!-- Subpage Header -->
+    <section class="" id="subpage-head">
+      <div class="flipbook-viewport">
+	    <div class="container">
+		  <div class="flipbook" >
+			<div >
+				<img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen01.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+			</div>
+        <div >
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen02.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div >
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen03.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div >
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen04.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div >
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen05.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen06.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen07.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen08.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen09.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen10.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen11.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen12.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen13.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen14.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen15.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen16.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen17.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen18.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen19.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen20.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen21.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen22.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen23.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen24.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen25.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen26.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen27.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen28.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen29.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+      <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen30.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen31.png'); ?>" class="" alt="Responsive image" width="100%" height="auto"> 
+      </div>
+         <div style="background-color:rgba(252, 127, 63, 1)">
+
+      </div>
+
+		  </div>
+		  </div>
+	    </div>
+    </section>
+      <!-- END Some About -->
+    </main>
+    <!-- END Main page content -->
