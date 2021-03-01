feb7b5f04bc5dc6a4f6f62669def6528f0d74d92
uziel1111 <uziel.123@gmail.com>
2021-03-01 10:40:24 -0600

    ajustes paginador modal

--- application/views/fotogaleria/index.php
+++ application/views/fotogaleria/index.php
@@ -523,7 +523,68 @@
         <div>
           <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen12.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
         </div>
-        </div>
+        <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen13.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen14.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen15.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen16.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen17.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen18.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen19.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen20.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen21.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen22.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen23.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen24.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen25.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen26.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen27.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen28.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen29.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen30.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+       <div>
+        <img src="<?= base_url('assets/img/xv_aniversario/foto_galeria/imagen31.png'); ?>" class="" alt="Responsive image" width="100%" height="auto">
+      </div>
+      <div style="background-color:#fc7f3f">
+
+      </div>
       </center>
         </div>
 
